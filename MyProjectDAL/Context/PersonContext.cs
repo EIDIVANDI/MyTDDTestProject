a623c634b961ea61e5d77d270387496efc443d25
Eidivandi <Eidivandi@live.com>
2016-12-19 23:52:05 +0100

    Add project files.

---
+++ MyProjectDAL/Context/PersonContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace MyProjectDAL.Context
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base()
+        { }
+
+        public virtual DbSet<Entity.Person> Persons { get; set; }
+
+    }
+}
---
+++ MyProjectDAL/Entity/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectDAL.Entity
+{
+    public class Person
+    {
+        public string PersonName { get; set; }
+
+        public string PersonEmail { get; set; }
+
+        public virtual Person Parent { get; set; }
+
+    }
+}
